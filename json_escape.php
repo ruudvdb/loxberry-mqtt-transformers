df90a9984d090aeb23ac4d91861ac98a8a2c056c
Ruud Van den Bosch <24699560+ruudvdb@users.noreply.github.com>
2024-11-19 09:37:20 +0100

    Create json_escape.php

---
+++ json_escape.php
@@ -0,0 +1,25 @@
+#!/usr/bin/php
+<?php
+	if( $argv[1] == 'skills' ) {
+		echo "description=Converting escaped double quotes to normal double quotes\n";
+		echo "link=\n";
+		echo "input=text\n";
+		echo "output=text\n";
+		exit();
+	}
+	
+	// ---- THIS CAN BE USED ALWAYS ----
+	// Remove the script name from parameters
+	array_shift($argv);
+	// Join together all command line arguments
+	$commandline = implode( ' ', $argv );	
+	// Split topic and data by separator
+	list( $topic, $data ) = explode( '#', $commandline, 2);
+	// ----------------------------------
+	
+	list($id, $state) = explode(' ', $data);
+
+	$data = str_replace('\"', '"', $data);
+	
+	echo $topic."#".$data."\n";
+	
